d4c6c8e06d7ae37fe24a53730357d640c3812432
Mialy14 <mialiniainacarine2@gmail.com>
2022-12-21 06:37:40 +0100

    token

--- src/main/java/gestion/helpers/TokenManager.java
+++ src/main/java/gestion/helpers/TokenManager.java
@@ -6,18 +6,9 @@ import java.sql.Date;
 import java.util.Random;
 
 public class TokenManager {
-    static String[] req;
-    public static  int genererInt(int borneInf, int borneSup){
-        Random random = new Random();
-        int nb;
-        nb = borneInf+random.nextInt(borneSup-borneInf);
-        return nb;
-    }
-       
     public static String generateToken(int id,String url) throws NoSuchAlgorithmException, UnsupportedEncodingException
     {
-        int numero= genererInt(0, url.length());
-        return Encrypte.SHA1(req[numero]+id+"  "+System.currentTimeMillis()+url);
+        return Encrypte.SHA1("messi pour tout"+id+"  "+System.currentTimeMillis()+url);
     }
     public static String getTok(String token)
     {
